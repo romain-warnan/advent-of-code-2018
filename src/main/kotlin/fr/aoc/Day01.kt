2481d3a0dd8ae9931a7c620faf4a53cf02b845b4
Romain Warnan <romain.warnan@gmail.com>
2018-12-01 14:08:24 +0100

    Day01, part 1: oneliner

--- src/main/kotlin/fr/aoc/Day01.kt
+++ src/main/kotlin/fr/aoc/Day01.kt
@@ -4,8 +4,8 @@ import java.io.File
 
 class Day01 {
 
-    fun part1(path: String): Long {
-        File(path).bufferedReader().lines()
-        return -1
-    }
+    fun part1(path: String) =  File(path).bufferedReader()
+        .lines()
+        .mapToLong { it.toLong() }
+        .sum()
 }
--- src/test/kotlin/fr/aoc/Day01Test.kt
+++ src/test/kotlin/fr/aoc/Day01Test.kt
@@ -10,6 +10,28 @@ class Day01Test {
     fun part1() {
         val answer = day.part1("src/main/resources/input01")
         println("Part 1 : $answer")
+        Assert.assertEquals(525, answer)
+    }
+
+    @Test
+    fun test1() {
+        val answer = day.part1("src/test/resources/input01.1")
+        Assert.assertEquals(3, answer)
+    }
+
+    @Test
+    fun test2() {
+        val answer = day.part1("src/test/resources/input01.2")
+        Assert.assertEquals(3, answer)
+    }
+    @Test
+    fun test3() {
+        val answer = day.part1("src/test/resources/input01.3")
         Assert.assertEquals(0, answer)
+    }
+    @Test
+    fun test4() {
+        val answer = day.part1("src/test/resources/input01.4")
+        Assert.assertEquals(-6, answer)
     }
 }
