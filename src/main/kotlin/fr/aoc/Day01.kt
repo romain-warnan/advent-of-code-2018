0d15cc12e2177ab1cb168b724dd3a94f5c6adfe9
Romain Warnan <romain.warnan@gmail.com>
2018-12-01 14:15:28 +0100

    Day02, part 2 moyen...

--- src/main/kotlin/fr/aoc/Day01.kt
+++ src/main/kotlin/fr/aoc/Day01.kt
@@ -1,6 +1,7 @@
 package fr.aoc
 
 import java.io.File
+import kotlin.streams.toList
 
 class Day01 {
 
@@ -8,4 +9,20 @@ class Day01 {
         .lines()
         .mapToLong { it.toLong() }
         .sum()
+
+    fun part2(path: String): Long {
+        val changes = File(path).bufferedReader()
+                .lines()
+                .mapToLong { it.toLong() }
+                .toList()
+        var frequency = 0L
+        var index = 0
+        val frequencies = mutableSetOf(frequency)
+        while (true) {
+            frequency += changes[index]
+            if (frequency in frequencies) return frequency
+            frequencies += frequency
+            index = (index + 1) % changes.size
+        }
+    }    
 }
