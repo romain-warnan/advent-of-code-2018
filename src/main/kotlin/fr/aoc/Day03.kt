9bd4d4be327bfd9add9daa71fee7618c58d89cf4
Romain Warnan <romain.warnan@gmail.com>
2018-12-03 07:10:52 +0100

    Day03, part2. Assez lent (13 s)

--- src/main/kotlin/fr/aoc/Day03.kt
+++ src/main/kotlin/fr/aoc/Day03.kt
@@ -5,13 +5,10 @@ import kotlin.streams.toList
 
 class Day03 {
 
-    val regex = Regex("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)")
+    private val regex = Regex("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)")
 
     fun part1(path: String): Int {
-        val claims = File(path).bufferedReader()
-            .lines()
-            .map { claim(it) }
-            .toList()
+        val claims = claims(path)
         val inches = mutableSetOf<Point>()
         for (i in claims.indices) {
             for (j in i + 1 until claims.size) {
@@ -21,6 +18,22 @@ class Day03 {
         }
         return inches.size
     }
+
+    fun part2(path: String): Int {
+        val claims = claims(path)
+        for (i in claims.indices) {
+            for (j in claims.indices) {
+                if(claims[i].overlaps(claims[j])) break;
+                if(j == claims.lastIndex) return claims[i].id
+            }
+        }
+        return -1
+    }
+
+    private fun claims(path: String) = File(path).bufferedReader()
+        .lines()
+        .map { claim(it) }
+        .toList()
 
     private fun claim(string: String): Claim {
         val (id, left, top, width, height) = regex.matchEntire(string)!!.destructured
@@ -41,6 +54,8 @@ data class Claim(val id: Int, val left: Int, val top: Int, val width: Int, val h
     }
 
     fun commonPoints(other: Claim) = this.points().intersect(other.points()).toSet()
+
+    fun overlaps(other: Claim) = this.id != other.id && commonPoints(other).isNotEmpty()
 }
 
 data class Point(val x: Int, val y: Int)
--- src/test/kotlin/fr/aoc/Day03Test.kt
+++ src/test/kotlin/fr/aoc/Day03Test.kt
@@ -14,8 +14,21 @@ class Day03Test {
     }
 
     @Test
+    fun part2() {
+        val answer = day.part2("src/main/resources/input03")
+        println("Part 2 : $answer")
+        Assert.assertEquals(707, answer)
+    }
+
+    @Test
     fun test1() {
         val answer = day.part1("src/test/resources/input03.1")
         Assert.assertEquals(4, answer)
+    }
+
+    @Test
+    fun test2() {
+        val answer = day.part2("src/test/resources/input03.1")
+        Assert.assertEquals(3, answer)
     }
 }
