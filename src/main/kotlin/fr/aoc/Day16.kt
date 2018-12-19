93eeb79142796346412c63cc8a2754c06396dd93
Romain Warnan <romain.warnan@insee.fr>
2018-12-19 09:38:48 +0100

    Day16, part1, moyennement propre

--- src/main/kotlin/fr/aoc/Day16.kt
+++ src/main/kotlin/fr/aoc/Day16.kt
@@ -1,116 +1,170 @@
 package fr.aoc
 
+import java.io.File
+
 class Day16 {
 
-    private val register = hashMapOf(
-        0 to 0,
-        1 to 0,
-        2 to 0,
-        3 to 0
-    )
+    private val register = mutableListOf(0, 0, 0, 0)
+
+    private val opcodes = listOf(
+        Addr(),
+        Addi(),
+        Mulr(),
+        Muli(),
+        Banr(),
+        Bani(),
+        Borr(),
+        Bori(),
+        Setr(),
+        Seti(),
+        Gtir(),
+        Gtri(),
+        Gtrr(),
+        Eqir(),
+        Eqri(),
+        Eqrr())
 
     fun part1(path: String): Int {
+        val monitorings = monitorings(path)
+        return monitorings.count { it.countMatches() >= 3 }
+    }
 
-        return -1
+    private fun monitorings(path: String): List<Monitoring> {
+        val monitorings = mutableListOf<Monitoring>()
+        val lines = File(path).readLines()
+        for(i in lines.indices step 4) {
+            monitorings += monitoring(lines.subList(i, i + 3))
+        }
+        return monitorings
+    }
+
+    private fun monitoring(lines: List<String>) = Monitoring(beforeOrAfter(lines[0]), beforeOrAfter(lines[2]), args(lines[1]))
+
+    private fun beforeOrAfter(line: String) = line
+        .substringAfter("[")
+        .substringBefore("]")
+        .replace(" ", "")
+        .split(",")
+        .map { it.toInt() }
+        .toList()
+
+    private fun args(line: String) = line
+        .split(" ")
+        .map { it.toInt() }
+        .toList()
+
+    inner class Monitoring(val before: List<Int>, val after: List<Int>, val args: List<Int>) {
+
+        val opcodeId = args[0]
+
+        private fun matches(opcode: Opcode): Boolean {
+            val register = before.toMutableList()
+            opcode.apply(register, args[1], args[2], args[3])
+            return register == after
+        }
+
+        fun countMatches() = opcodes.count { matches(it) }
+
+        fun matchesOpcodes() = opcodes.filter { matches(it) }.toList()
     }
 
     interface Opcode {
-        fun apply(A: Int, B: Int, C: Int)
+        fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int)
     }
 
-    inner class Addr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! + register[B]!!
+    class Addr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] + register[B]
         }
     }
 
-    inner class Addi() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! + B
+    class Addi : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] + B
         }
     }
 
-    inner class Mulr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! * register[B]!!
+    class Mulr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] * register[B]
         }
     }
 
-    inner class Muli() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! * B
+    class Muli : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] * B
         }
     }
 
-    inner class Banr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! and register[B]!!
+    class Banr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] and register[B]
         }
     }
 
-    inner class Bani() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! and B
+    class Bani : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] and B
         }
     }
 
-    inner class Borr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! or register[B]!!
+    class Borr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] or register[B]
         }
     }
 
-    inner class Bori() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!! or B
+    class Bori : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A] or B
         }
     }
 
-    inner class Setr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            register[C] = register[A]!!
+    class Setr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            register[C] = register[A]
         }
     }
 
-    inner class Seti() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
+    class Seti : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
             register[C] = A
         }
     }
 
-    inner class Gtir() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            if(A >= register[B]!!) register[C] = 1 else register[C] = 0
+    class Gtir : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            if(A > register[B]) register[C] = 1 else register[C] = 0
         }
     }
 
-    inner class Gtri() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            if(register[A]!! >= B) register[C] = 1 else register[C] = 0
+    class Gtri : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            if(register[A] > B) register[C] = 1 else register[C] = 0
         }
     }
 
-    inner class Gtrr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            if(register[A]!! >= register[B]!!) register[C] = 1 else register[C] = 0
+    class Gtrr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            if(register[A] > register[B]) register[C] = 1 else register[C] = 0
         }
     }
 
-    inner class Eqir() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            if(A == register[B]!!) register[C] = 1 else register[C] = 0
+    class Eqir : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            if(A == register[B]) register[C] = 1 else register[C] = 0
         }
     }
 
-    inner class Eqri() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            if(register[A]!! == B) register[C] = 1 else register[C] = 0
+    class Eqri : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            if(register[A] == B) register[C] = 1 else register[C] = 0
         }
     }
 
-    inner class Eqrr() : Opcode {
-        override fun apply(A: Int, B: Int, C: Int) {
-            if(register[A]!! == register[B]!!) register[C] = 1 else register[C] = 0
+    class Eqrr : Opcode {
+        override fun apply(register: MutableList<Int>, A: Int, B: Int, C: Int) {
+            if(register[A] == register[B]) register[C] = 1 else register[C] = 0
         }
     }
 }
