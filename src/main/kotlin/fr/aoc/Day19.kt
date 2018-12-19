61da809aec08495acdc5624cf6d557ff6279d92e
Romain Warnan <romain.warnan@insee.fr>
2018-12-19 12:50:05 +0100

    Mise en place Day19

---
+++ src/main/kotlin/fr/aoc/Day19.kt
@@ -0,0 +1,10 @@
+package fr.aoc
+
+class Day19 {
+
+    fun part1(ip: Int, path: String): Int {
+        val register = mutableListOf(0, 0, 0, 0, 0, 0)
+
+        return register[0]
+    }
+}
---
+++ src/test/kotlin/fr/aoc/Day19Test.kt
@@ -0,0 +1,21 @@
+package fr.aoc
+import org.junit.Assert
+import org.junit.Test
+
+class Day19Test {
+
+    private val day = Day19()
+
+    @Test
+    fun part1() {
+        val answer = day.part1(3, "src/main/resources/input19")
+        println("Part 1 : $answer")
+        Assert.assertEquals(-1, answer)
+    }
+
+    @Test
+    fun test1() {
+        val answer = day.part1(0, "src/test/resources/input19.1")
+        Assert.assertEquals(6, answer)
+    }
+}
