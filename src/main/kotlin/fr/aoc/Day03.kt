2b8f059567fa0c08458cde082c5a26921bed32aa
Romain Warnan <romain.warnan@gmail.com>
2019-01-26 17:09:24 +0100

    Optimisation du Day03

--- src/main/kotlin/fr/aoc/Day03.kt
+++ src/main/kotlin/fr/aoc/Day03.kt
@@ -2,6 +2,8 @@ package fr.aoc
 
 import java.io.File
 import kotlin.streams.toList
+import java.util.Optional
+import kotlin.math.min
 
 class Day03 {
 
@@ -37,26 +39,49 @@ class Day03 {
 
     private fun claim(string: String): Claim {
         val (id, left, top, width, height) = regex.matchEntire(string)!!.destructured
-        return Claim(id.toInt(), left.toInt(), top.toInt(), width.toInt(), height.toInt())
+        return Claim(id.toInt(), Segment(top.toInt(), height.toInt()), Segment(left.toInt(), width.toInt()))
     }
 
-    data class Claim(val id: Int, val left: Int, val top: Int, val width: Int, val height: Int) {
+    data class Claim(val id: Int, private val height: Segment , private val width: Segment) {
+
+        private fun intersection(other: Claim): Optional<Claim> {
+            val intersectionHeight = this.height.intersection(other.height)
+            val intersectionWidth = this.width.intersection(other.width)
+            return if (intersectionHeight.isPresent && intersectionWidth.isPresent) {
+                Optional.of(Claim(0, intersectionHeight.get(), intersectionWidth.get()))
+            } else {
+                Optional.empty()
+            }
+        }
 
         private fun points(): Set<Point> {
             val points = mutableSetOf<Point>()
-            for (x in left until left + width) {
-                for (y in top until top + height) {
-                    points += Point(x, y)
+            for (j in 0 until height.length) {
+                for (i in 0 until width.length) {
+                    points.add(Point(width.start + i, height.start + j))
                 }
             }
             return points
         }
 
-        fun commonPoints(other: Claim) = this.points().intersect(other.points()).toSet()
+        fun commonPoints(other: Claim): Set<Point> = this.intersection(other)
+            .map{ it.points() }
+            .orElse(setOf())
 
         fun overlaps(other: Claim) = this.id != other.id && commonPoints(other).isNotEmpty()
 
         data class Point(val x: Int, val y: Int)
+    }
+
+    data class Segment(val start: Int, val length: Int) {
+
+        private val end = start + length -1
+
+        fun intersection(other: Segment) = when {
+            this.start <= other.start && other.start <= this.end -> Optional.of(Segment(other.start, min(this.end, other.end) + 1 - other.start))
+            other.start <= this.start && this.start <= other.end -> Optional.of(Segment(this.start, min(this.end, other.end) + 1 - this.start))
+            else -> Optional.empty()
+        }
     }
 }
 
